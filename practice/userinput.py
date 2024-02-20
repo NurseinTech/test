bb999a1d8722d5fde87592992daff934d1d25671
NurseinTech <odhiamboandrew78@gmail.com>
2024-02-20 10:28:12 +0300

    asking user input

---
+++ practice/userinput.py
@@ -0,0 +1,6 @@
+#ask userinput
+name = input('enter your name ')
+age = input('Enter your age')
+location = input('Enter your location')
+#print the output 
+print('your name is{}, and your age is {}, and your location is{}'.format(name, age, location))
